b619c5a29ac8270a839e226bd06fc47dd5aad379
Chris Segura <chris.segura@live.com>
2016-02-15 20:33:27 -0800

    a bunch of prototypes, but migh fork code to clean up VS project file quickly

--- wssccatIotDashboard/wssccatIotDashboard/Models/SensorData.cs
+++ wssccatIotDashboard/wssccatIotDashboard/Models/SensorData.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web.Http.Filters;
 using System.Web;
@@ -17,34 +18,45 @@ namespace wssccatIotDashboard.Models
         public int Humidity { get; set; } 
         public string TimeStamp { get; set; } 
         public string ClientName { get; set; }
-        private async Task PostDataAsync()
+        public async Task CreateConsumer()
         {
-            string topicString = "/SensorData";
-            UriBuilder u1 = new UriBuilder();
-            //u1.Host = "localhost"; //DEBUG
-            u1.Host = "wssccatiot.westus.cloudapp.azure.com";
-            u1.Port = 8082;
-            u1.Path = "topics" + topicString;
-            u1.Scheme = "http";
-            Uri topicUri = u1.Uri;
+            //string topicString = "/SensorData";
+            //UriBuilder u1 = new UriBuilder();
+            ////u1.Host = "localhost"; //DEBUG
+            //u1.Host = "wssccatiot.westus.cloudapp.azure.com";
+            //u1.Port = 8082;
+            //u1.Path = "topics" + topicString;
+            //u1.Scheme = "http";
+            //Uri topicUri = u1.Uri;
+            string topicUri = "http://wssccatiot.westus.cloudapp.azure.com:8082";
             //Currently focused on REST API surface for Confluent.io Kafka deployment. We can make this more generic in the future
-            string jsonBody = JsonConvert.SerializeObject(data, Formatting.None);
+            //DEBUG
+            //string jsonBody = JsonConvert.SerializeObject(data, Formatting.None);
+            //END DEBUG
+            string jsonBody = "stuff goes here";
             //string correctedJsonBody = jsonBody.Replace(",", "}}, {\"value\":{"); //have to add in some json features into the string. Easier than creating unnecessary classes that would make this come out automatically
-            jsonBody = jsonBody.Replace(",", "}}, {\"value\":{"); //have to add in some json features into the string. Easier than creating unnecessary classes that would make this come out automatically
+            //jsonBody = jsonBody.Replace(",", "}}, {\"value\":{"); //have to add in some json features into the string. Easier than creating unnecessary classes that would make this come out automatically
             string jsonHeader = ("{\"records\":[{\"value\":"); //same as above, fixing string for Server requirements
             string jsonFooter = ("}]}"); //ditto
             string json = jsonHeader + jsonBody + jsonFooter;
 
-            var baseFilter = new HttpBaseProtocolFilter();
-            baseFilter.AutomaticDecompression = false; //turn OFF header "Accept-Encoding"
+            var baseFilter = new HttpClientHandler();
+            baseFilter.AutomaticDecompression = System.Net.DecompressionMethods.None; //turn off all compression methods
             HttpClient httpClient = new HttpClient(baseFilter);
             try
             {
-                var headerContent = new HttpStringContent(json);
-                headerContent.Headers.ContentType = null; // removing all header content and will replace with the required values
-                headerContent.Headers.ContentType = new HttpMediaTypeHeaderValue("application/vnd.kafka.json.v1+json"); //Content-Type: application/vnd.kafka.json.v1+json
-                httpClient.DefaultRequestHeaders.Accept.Add(new HttpMediaTypeWithQualityHeaderValue("application/vnd.kafka.json.v1+json, application/vnd.kafka+json, application/json")); //Add Accept: application/vnd.kafka.json.vl+json, application... header
-                HttpResponseMessage postResponse = await httpClient.PostAsync(topicUri, headerContent);
+                //httpClient.BaseAddress = topicUri;
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.AcceptEncoding.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.kafka.json.v1+json, application/vnd.kafka+json, application/json")); //Add Accept: application/vnd.kafka.json.vl+json, application... header )
+                HttpResponseMessage postResponse = await httpClient.PostAsync(topicUri, new StringContent(json));
+                //FROM IOT CLIENT SIDE CODE - DELETE ONCE THE SERVER SIDE CODE WORKS
+                //var headerContent = new HttpStringContent(json);
+                //headerContent.Headers.ContentType = null; // removing all header content and will replace with the required values
+                //headerContent.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/vnd.kafka.json.v1+json"); //Content-Type: application/vnd.kafka.json.v1+json
+                //httpClient.DefaultRequestHeaders.Accept.Add(new HttpMediaTypeWithQualityHeaderValue("application/vnd.kafka.json.v1+json, application/vnd.kafka+json, application/json")); //Add Accept: application/vnd.kafka.json.vl+json, application... header
+                //HttpResponseMessage postResponse = await httpClient.PostAsync(topicUri, headerContent);
+                //END CLIENT SIDE CODE SNIPPET
             }
             catch
             {
